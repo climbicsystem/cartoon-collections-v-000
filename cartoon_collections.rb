32020aa8fd83ec1e93859ba4705292292a6d4549
Joseph Vint <josephvint@me.com>
2018-02-12 21:53:46 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  veggies.collect do |element|
+  planeteer_calls.collect do |element|
     element = element.capitalize + "!"
   end
 end
